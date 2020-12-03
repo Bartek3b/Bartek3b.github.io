037813f9114258aa1c8fee9aed46932e68818b60
Bartek3b <73472410+Bartek3b@users.noreply.github.com>
2020-12-03 20:48:16 +0100

    added js and images compressed

--- js/gallery.js
+++ js/gallery.js
@@ -1,7 +1,31 @@
-// function zoom(){
-//     document.getElementById("img").style.transform="scale(4)";
-// }
+let galleryImages = document.querySelectorAll(".images-gallery");
+let getLatestOpenedImg;
+let windowWidth = window.innerWith;
 
-function skala(){
-    document.getElementById("img").classList.toggle("zoomout");
+if(galleryImages){
+    galleryImages.forEach(function(image, index){
+        image.onclick = function() {
+            let getElementCss = window.getComputedStyle(image);
+            let getFullImgUrl = getElementCss.getPropertyValue("background-image");
+            let getImgUrlPos = getFullImgUrl.split("images");
+            let setNewImgUrl = getImgUrlPos[1].replace('")','');
+
+            getLatestOpenedImg = index + 1;
+
+            let container = document.body;
+            let newImgWindow = document.createElement("div");
+            container.appendChild(newImgWindow);
+            newImgWindow.setAttribute("class", "img-window");
+            newImgWindow.setAttribute("onclick", "closeImg()");
+
+            let newImg = document.createElement("img");
+            newImgWindow.appendChild(newImg);
+            newImg.setAttribute("src", "/images" + setNewImgUrl);
+        }
+    });
+}
+
+function closeImg(){
+    document.querySelector(".img-window").remove();
+
 }
