e8a219773646ddaffce89420ef48748dbd25d528
Bartek3b <73472410+Bartek3b@users.noreply.github.com>
2020-12-06 17:37:06 +0100

    delete useless js

--- js/tlo.js
+++
@@ -1,7 +0,0 @@
-let container = document.getElementById('container');
-window.onmousemove = function (e) {
-    let x = -e.clientX / 15,
-        y = -e.clientY / 9;
-    container.style.backgroundPositionX = x + 'px';
-    container.style.backgroundPositionY = y + 'px';
-}
